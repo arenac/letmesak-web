17a5ea3bea00cea3d5743a8994541868b36767fb
Nilo Neregato <nilo.neregato@scania.com>
2021-08-18 15:59:28 +0200

    Access created room

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,4 @@
+import { FormEvent, useState } from 'react';
 import { useHistory } from 'react-router-dom'
 
 import illustrationImg from "assets/images/illustration.svg"
@@ -7,8 +8,10 @@ import googleIconImg from "assets/images/google-icon.svg"
 import "styles/auth.scss";
 import { Button } from "components/Button";
 import { useAuth } from 'hooks/useAuth';
+import { database } from 'services/firebase';
 
 export function Home() {
+  const [roomCode, setRoomCode] = useState('')
   const history = useHistory()
 
  const { user, signInWithGoogle } = useAuth()
@@ -18,6 +21,23 @@ export function Home() {
       await signInWithGoogle()
     }
     history.push("/rooms/new")
+  }
+
+  async function handleJoinRoom(event: FormEvent) {
+    event.preventDefault()
+
+    if(roomCode.trim() === '') {
+      return
+    }
+
+    const roomRef = await database.ref(`rooms/${roomCode}`).get()
+
+    if(!roomRef.exists()) {
+      alert('Room does not exist.')
+      return
+    }
+
+    history.push(`rooms/${roomRef.key}`) 
   }
 
   return (
@@ -37,10 +57,12 @@ export function Home() {
 
           <div className="separator">or enter in an existing chat room</div>
 
-          <form>
+          <form onSubmit={handleJoinRoom}>
             <input 
               type="text" 
               placeholder="Enter the room code"
+              value={roomCode}
+              onChange={event => setRoomCode(event.target.value)}
             />
             <Button type="submit">
               Enter
