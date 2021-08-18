a35472b24dd29f70114a47b372b7b48a93cd4c18
Nilo Neregato <nilo.neregato@scania.com>
2021-08-18 20:06:56 +0200

    Room: - layout and styles - RoomCode component

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,3 +1,40 @@
+import { useParams } from 'react-router-dom'
+import logoImg from 'assets/images/logo.svg'
+import { Button } from 'components/Button'
+import { RoomCode } from 'components/RoomCode'
+
+import 'styles/rooms.scss'
+
+type RoomParams = {
+  id: string;
+}
+
 export function Room() {
-  return <h1>Room pages</h1>
+  const params = useParams<RoomParams>()
+
+  return (
+    <div id="page-room">
+      <header>
+        <div className="content">
+          <img src={logoImg} alt="Letmeask" />
+          <RoomCode code={params.id}/>
+        </div>
+      </header>
+      <main>
+        <div className="room-title">
+          <h1>React room</h1>
+          <span>4 questions</span>
+        </div>
+
+        <form>
+          <textarea placeholder="Your question"/>
+
+          <div className="from-footer">
+            <span>To ask question, <button>login here</button>.</span>
+            <Button type="submit">Send question</Button>
+          </div>
+        </form>
+      </main>
+    </div>  
+  )
 }
